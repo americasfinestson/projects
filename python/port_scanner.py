0d3194940f8e13c2bf0ee60a26d9cbb405c38a8e
Josh Whipkey <americasfinestson@gmail.com>
2022-02-16 16:19:34 -0700

    Updates to port scanner

--- python/port_scanner.py
+++ python/port_scanner.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 
-import os
-import sys
+""" Port scans localhost and prints open ports
+
+Arguments: None
+
+Outputs: None
+
+"""
+
 import socket
 
 
 def main():
 
-    # Define machine to port scan
-    machine = socket.gethostbyname('localhost')
+    # Get the IPv4 address of localhost
+    ip = socket.gethostbyname('localhost')
 
     for p in range(1,65535):
+        # Initiate a socket instance using IPv4 and TCP
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
+        # Set default timeout to 0.1s
+        socket.setdefaulttimeout(0.1)
 
-        connection = s.connect_ex((machine, p))
+        # connect_ex is like connect, but returns an error code instead of exception object
+        connection = s.connect_ex((ip, p))
         if connection == 0:
-            print("Successfully connected to {}:{}".format(machine, p))
+            print("Successfully connected to {}:{}".format(ip, p))
         s.close()
     
 
